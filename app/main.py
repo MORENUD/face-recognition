fdab5aa0a43cbc4f4fb7fe7365c75cae50e4e37d
Peerapat Tancharoen <129715744+peerapat-t@users.noreply.github.com>
2025-12-13 13:24:44 +0700

    mock endpoint

--- app/main.py
+++ app/main.py
@@ -105,5 +105,58 @@ async def recognize_face(payload: ImageRequest):
         }
     }
 
+
+@app.post("/recognize_mock", response_model=PredictionResponse)
+async def recognize_face(payload: ImageRequest):
+    known_database = ml_resources.get("known_database")
+    if known_database is None:
+        known_database = {}
+
+    try:
+        input_image = decode_base64_image(payload.image_base64)
+        target_encoding = get_face_embedding(input_image)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Image processing failed: {str(e)}")
+
+    if target_encoding is None:
+        return {
+            "name": "No Face Detected",
+            "disease": "NA",
+            "current_appointment": '2099-12-31',
+            "debug_info": {"file": "NA", "score": 0.0}
+        }
+
+    closest_filename, score, is_match = find_match(target_encoding, known_database, threshold=0.40)
+
+    if not is_match:
+        return {
+            "name": "Unknown",
+            "disease": "NA",
+            "current_appointment": '2099-12-31',
+            "debug_info": {
+                "file": closest_filename,
+                "score": round(score, 4)
+            }
+        }
+
+    real_name = os.path.splitext(closest_filename)[0]
+    patient_info = get_patient_info(closest_filename)
+
+    disease = patient_info["disease"] if patient_info else "Data Not Found"
+    appt_day = patient_info["current_appointment"] if patient_info else "2099-12-31"
+
+    return {
+        "name": real_name,
+        "disease": disease,
+        "current_appointment": appt_day,
+        "debug_info": {
+            "file": closest_filename,
+            "score": round(score, 4)
+        }
+    }
+
+
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
